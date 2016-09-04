e72176af1ee385bf3ee652ad33b391fff9736c3c
wojcekq <rafa@Rafas-Mac-mini.local>
2016-09-04 09:47:38 +0200

    server.js modified

--- server.js
+++ server.js
@@ -2,21 +2,11 @@ var express = require('express');
 var app = express();
 var PORT = 3000;
 
+var middleware = require('./middleware.js');
+
 //app.get('/', function(request,response){
 //    response.send('Hello Express');
 //});
-
-var middleware = {
-    requireAuthentication: function(request,response, next){
-        console.log('private route hit');
-        next();
-    },
-    logger: function(request, response, next){
-        //new Date().toString()
-        console.log('Request: ' + new Date().toString() + ' ' + request.method + ' ' + request.originalUrl);
-        next();
-    }
-};
 
 app.use(middleware.logger);
 
