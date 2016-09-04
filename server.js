85b87e446abe2ffddb9e7a22b4bcde927a0cb0f7
wojcekq <rafa@Rafas-Mac-mini.local>
2016-09-04 09:43:23 +0200

    server.js modified

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ var middleware = {
 app.use(middleware.logger);
 
 app.get('/about', middleware.requireAuthentication, function(request,response){
-    response.send('About Us');
+    response.send('About Us!');
 });
 
 app.use(express.static(__dirname + '/public'));
